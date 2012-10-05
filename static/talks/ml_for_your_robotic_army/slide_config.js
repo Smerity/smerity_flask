e59ef9cc6eab7b65dcf0f12e79ff683164d4f5f2
Stephen Merity <smerity@smerity.com>
2012-10-05 22:59:52 +1000

    Modified consulting and about me pages, added my SyPy talk

---
+++ static/talks/ml_for_your_robotic_army/slide_config.js
@@ -0,0 +1,35 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Machine Learning<br>for your Robotic Army',
+    subtitle: "A Crash Course using Python's Scikit-Learn",
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Stephen Merity',
+    company: 'smerity@smerity.com',
+    twitter: '@smerity',
+    www: 'http://www.smerity.com',
+    github: 'http://github.com/smerity'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
